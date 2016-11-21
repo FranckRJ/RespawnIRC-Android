6cf903d03045e3f4272ebe6a620a59a9dd0f59bf
LEpigeon888 <LEpigeon888@gmail.com>
2016-11-21 02:19:13 +0100

    - passage d'un attribut protected à public.

--- java/com/franckrj/respawnirc/SettingsActivity.java
+++ java/com/franckrj/respawnirc/SettingsActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
