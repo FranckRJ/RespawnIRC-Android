f00e4883d9fa8ce8330614ffcc6d997afa4ab21b
LEpigeon888 <LEpigeon888@gmail.com>
2017-02-15 20:16:07 +0100

    - correction d'un bug empêchant de citer un message.

--- java/com/franckrj/respawnirc/MainActivity.java
+++ java/com/franckrj/respawnirc/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        System.setProperty("http.keepAlive", "false");
+        System.setProperty("http.keepAlive", "true");
         WebManager.generateNewUserAgent();
         StickerConverter.initializeBasesRules();
 
