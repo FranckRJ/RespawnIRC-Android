677de0089a42c1e9afd8bf2b9de5d7be6e9ac3a6
LEpigeon888 <LEpigeon888@gmail.com>
2017-04-02 21:07:03 +0200

    - changement de la couleur de la TaskDescription selon le thème.

--- java/com/franckrj/respawnirc/ThemedActivity.java
+++ java/com/franckrj/respawnirc/ThemedActivity.java
@@ -1,11 +1,18 @@
 package com.franckrj.respawnirc;
 
+import android.app.ActivityManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.franckrj.respawnirc.utils.ThemeManager;
+import com.franckrj.respawnirc.utils.Undeprecator;
 
 public class ThemedActivity extends AppCompatActivity {
+    protected static ActivityManager.TaskDescription generalTaskDesc = null;
+    protected static ThemeManager.ThemeName themeUsedForGenerateTaskDesc = null;
     protected ThemeManager.ThemeName oldThemeUsed = null;
 
     @Override
@@ -13,14 +20,22 @@ public class ThemedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         oldThemeUsed = ThemeManager.getThemeUsed();
         ThemeManager.changeActivityTheme(this);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            if (generalTaskDesc == null || themeUsedForGenerateTaskDesc != ThemeManager.getThemeUsed()) {
+                Bitmap appIcon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_rirc);
+                generalTaskDesc = new ActivityManager.TaskDescription(getString(R.string.app_name), appIcon, Undeprecator.resourcesGetColor(getResources(), ThemeManager.getColorRes(ThemeManager.ColorName.COLOR_PRIMARY)));
+                themeUsedForGenerateTaskDesc = ThemeManager.getThemeUsed();
+            }
+            setTaskDescription(generalTaskDesc);
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        ThemeManager.ThemeName currentTheme = ThemeManager.getThemeUsed();
 
-        if (oldThemeUsed != null && currentTheme != oldThemeUsed) {
+        if (oldThemeUsed != null && oldThemeUsed != ThemeManager.getThemeUsed()) {
             recreate();
         }
     }
