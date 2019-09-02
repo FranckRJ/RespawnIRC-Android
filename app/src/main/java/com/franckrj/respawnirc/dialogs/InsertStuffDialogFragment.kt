b4765f0ce04fda6de369bc227524cf233d86f077
Franck W <franck.rj.w@gmail.com>
2019-09-02 17:03:38 +0200

    Passage au databinding pour le dialog InsertStuff.

--- app/src/main/java/com/franckrj/respawnirc/dialogs/InsertStuffDialogFragment.kt
+++ app/src/main/java/com/franckrj/respawnirc/dialogs/InsertStuffDialogFragment.kt
@@ -1,15 +1,14 @@
 package com.franckrj.respawnirc.dialogs
 
-import android.annotation.SuppressLint
 import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import android.widget.ScrollView
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.franckrj.respawnirc.R
+import com.franckrj.respawnirc.databinding.DialogInsertstuffBinding
 import com.franckrj.respawnirc.utils.PrefsManager
 import com.franckrj.respawnirc.utils.ThemeManager
 
@@ -17,7 +16,7 @@ class InsertStuffDialogFragment : DialogFragment() {
     private val listOfCategoryButtons = mutableListOf<ImageView>()
     private var oldRowNumber: Int = 1
 
-    private lateinit var scrollViewOfButtons: ScrollView
+    private lateinit var bindings: DialogInsertstuffBinding
 
     private val categoryButtonClickedListener = View.OnClickListener { view ->
         var newRowNumber: Int = (view.tag as? Int) ?: 0 // est-ce réellement une bonne idée de cacher l'exception ?
@@ -37,7 +36,10 @@ class InsertStuffDialogFragment : DialogFragment() {
         )
         /*initializeSpanForTextViewIfNeeded(jvcImageGetter, rowToUse)
         mainTextView.setText(replaceUrlSpans(listOfSpanForTextView[rowToUse]))*/
-        scrollViewOfButtons.requestChildFocus(listOfCategoryButtons[rowToUse], listOfCategoryButtons[rowToUse])
+        bindings.listScrollviewInsertstuff.requestChildFocus(
+            listOfCategoryButtons[rowToUse],
+            listOfCategoryButtons[rowToUse]
+        )
         oldRowNumber = rowToUse
         PrefsManager.putInt(PrefsManager.IntPref.Names.LAST_ROW_SELECTED_INSERTSTUFF, oldRowNumber)
         PrefsManager.applyChanges()
@@ -45,28 +47,27 @@ class InsertStuffDialogFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(requireActivity())
-        @SuppressLint("InflateParams")
-        val mainView = requireActivity().layoutInflater.inflate(R.layout.dialog_insertstuff, null)
 
-        scrollViewOfButtons = mainView.findViewById(R.id.list_scrollview_insertstuff)
+        bindings = DialogInsertstuffBinding.inflate(requireActivity().layoutInflater)
+        bindings.dialog = this
 
-        listOfCategoryButtons.add(mainView.findViewById(R.id.smiley_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.textformat_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_1_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_2_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_3_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_4_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_5_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_6_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_7_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_8_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_9_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_10_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_11_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_12_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_13_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_14_button_insertstuff))
-        listOfCategoryButtons.add(mainView.findViewById(R.id.sticker_15_button_insertstuff))
+        listOfCategoryButtons.add(bindings.smileyButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.textformatButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker1ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker2ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker3ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker4ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker5ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker6ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker7ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker8ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker9ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker10ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker11ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker12ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker13ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker14ButtonInsertstuff)
+        listOfCategoryButtons.add(bindings.sticker15ButtonInsertstuff)
 
         for (i in listOfCategoryButtons.indices) {
             listOfCategoryButtons[i].tag = i
@@ -81,8 +82,8 @@ class InsertStuffDialogFragment : DialogFragment() {
 
         selectThisRow(oldRowNumber)
 
-        builder.setTitle(R.string.insertStuff).setView(mainView)
-            .setNegativeButton(R.string.cancel) { dialog, id -> dialog.dismiss() }
+        builder.setTitle(R.string.insertStuff).setView(bindings.root)
+            .setNegativeButton(R.string.cancel) { dialog, _ -> dialog.dismiss() }
 
         return builder.create()
     }
